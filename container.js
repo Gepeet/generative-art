a8555bd001428f8e3ea3ec4b3bd788f84f18f9d9
VenGallardo <letskeddotcom@gmail.com>
2021-09-23 10:06:37 +0800

    refactored js file

--- container.js
+++ container.js
@@ -1,21 +1,19 @@
-let box = document.querySelector('#box');
-let box2 = document.querySelector('#box2');
-let box3 = document.querySelector('#box3');
-let box4 = document.querySelector('#box4');
-let box5 = document.querySelector('#box5');
-let box6 = document.querySelector('#box6');
+const box = document.querySelector('#box');
+const box2 = document.querySelector('#box2');
+const box3 = document.querySelector('#box3');
+const box4 = document.querySelector('#box4');
+const box5 = document.querySelector('#box5');
+const box6 = document.querySelector('#box6');
 
-box.style.width = Math.floor(Math.random() * 500 + 1) +"px";
-box.style.height = Math.floor(Math.random() * 500 + 1) +"px";
-box2.style.width = Math.floor(Math.random() * 500 + 1) +"px";
-box2.style.height = Math.floor(Math.random() * 500 + 1) +"px";
-box3.style.width = Math.floor(Math.random() * 500 + 1) +"px";
-box3.style.height = Math.floor(Math.random() * 500 + 1) +"px";
-box4.style.width = Math.floor(Math.random() * 500 + 1) +"px";
-box4.style.height = Math.floor(Math.random() * 500 + 1) +"px";
-box5.style.width = Math.floor(Math.random() * 500 + 1) +"px";
-box5.style.height = Math.floor(Math.random() * 500 + 1) +"px";
-box6.style.width = Math.floor(Math.random() * 500 + 1) +"px";
-box6.style.height = Math.floor(Math.random() * 500 + 1) +"px";
 
-console.log(box2,box3)
+
+changeCss = () =>{
+    const allBox = [box,box2,box3,box4,box5,box6];
+    for(let i = 0 ; i < allBox.length ; i++){
+        allBox[i].style.width = Math.floor(Math.random() * 500 + 1) +"px";
+        allBox[i].style.height = Math.floor(Math.random() * 500 + 1) +"px";
+        allBox[i].style.background = "#" + Math.floor(Math.random()*16777215).toString(16);
+    }
+}
+
+changeCss()
