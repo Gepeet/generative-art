cbf99996da6cda767c24a6732ca1540a42c459d1
VenGallardo <letskeddotcom@gmail.com>
2021-09-23 09:32:08 +0800

    created box art

---
+++ container.js
@@ -0,0 +1,21 @@
+let box = document.querySelector('#box');
+let box2 = document.querySelector('#box2');
+let box3 = document.querySelector('#box3');
+let box4 = document.querySelector('#box4');
+let box5 = document.querySelector('#box5');
+let box6 = document.querySelector('#box6');
+
+box.style.width = Math.floor(Math.random() * 500 + 1) +"px";
+box.style.height = Math.floor(Math.random() * 500 + 1) +"px";
+box2.style.width = Math.floor(Math.random() * 500 + 1) +"px";
+box2.style.height = Math.floor(Math.random() * 500 + 1) +"px";
+box3.style.width = Math.floor(Math.random() * 500 + 1) +"px";
+box3.style.height = Math.floor(Math.random() * 500 + 1) +"px";
+box4.style.width = Math.floor(Math.random() * 500 + 1) +"px";
+box4.style.height = Math.floor(Math.random() * 500 + 1) +"px";
+box5.style.width = Math.floor(Math.random() * 500 + 1) +"px";
+box5.style.height = Math.floor(Math.random() * 500 + 1) +"px";
+box6.style.width = Math.floor(Math.random() * 500 + 1) +"px";
+box6.style.height = Math.floor(Math.random() * 500 + 1) +"px";
+
+console.log(box2,box3)
